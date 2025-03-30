e4f1b59c92a9b94dcd5d85dcadbe626b887abaf1
FOREST <forest310125@gmail.com>
2025-03-30 08:27:58 -0400

    Update deploy.js

--- migrations/deploy.js
+++ migrations/deploy.js
@@ -7,6 +7,5 @@ const anchor = require("@project-serum/anchor");
 module.exports = async function (provider) {
   // Configure client to use the provider.
   anchor.setProvider(provider);
-
   // Add your deploy script here.
 }
